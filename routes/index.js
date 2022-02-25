b75f9f09440636a78729ffaf33e21fee30d347d8
Huy Nguyen <huy.nq@geekup.vn>
2022-02-25 11:00:00 +0700

    Update logic & router

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,22 @@ const NotionPageToHtml = require('notion-page-to-html');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/notion-to-html', async function(req, res, next) {
+router.get('/html/:notionKey', async function(req, res, next) {
   try {
-    if (!req.query.id) {
-      res.status(400).send('Error: Missing \'id\'');
+    const {notionKey} = req.params;
+    if (!notionKey) {
+      res.status(400).send('Error: Missing \'notion-key\'');
       return;
     }
-    console.log('Get content for id', req.query.id);
-    const url = `https://notion.so/${req.query.id}`;
+    const url = notionKey.includes('https://') ?
+      notionKey :
+      `https://notion.so/${notionKey}`;
     const content = await NotionPageToHtml.convert(url);
     if (!content) {
       res.send(`Error: Convert Notion to HTML from '${url}'`);
       return;
     }
     const {html} = content;
-
 
     res.setHeader('Content-Type', 'text/html');
     res.status(200).send(html);
